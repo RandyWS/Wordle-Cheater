20c4d9a4eda0a214e53659704043bc957b9f426a
Miranda Stopa <miranda.stopa@gmail.com>
2022-01-10 17:14:27 -0800

    filtering by letters working

--- client/store/words.js
+++ client/store/words.js
@@ -14,12 +14,15 @@ export const setFetchedWords = (words) => {
 export const _fetchWords = (query) => {
   return async (dispatch) => {
     try {
+      // base query
       const base = "https://api.datamuse.com/words?sp=";
       let words = [];
 
+      // calls the datamuse api, returns arr of objects
+      // filter the objects by just the word
       const newPromise = new Promise((res, rej) => {
         res(
-          fetch("https://api.datamuse.com/words?sp=?????")
+          fetch("https://api.datamuse.com/words?sp=?????&max=1000")
             .then((res) => res.json())
             .then((res) => {
               res.map((resObj) => words.push(resObj.word));
@@ -30,8 +33,26 @@ export const _fetchWords = (query) => {
       await newPromise;
 
       if (words.length) {
-        dispatch(setFetchedWords(words));
+        // pass words to our filter that keeps track of excluded letters
+        dispatch(filterWords(words));
       }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export const filterWords = (wordsArr) => {
+  return async (dispatch, getState) => {
+    try {
+      const { words } = getState();
+      let filteredWords = [...wordsArr];
+
+      words.excludedLetters.map((letter) => {
+        filteredWords = filteredWords.filter((word) => !word.includes(letter));
+      });
+
+      dispatch(setFetchedWords(filteredWords));
     } catch (error) {
       console.log(error);
     }
@@ -41,6 +62,7 @@ export const _fetchWords = (query) => {
 const initialState = {
   guessedWords: [],
   possibleWords: [],
+  excludedLetters: ["a", "i"],
 };
 
 export default function (state = initialState, action) {
